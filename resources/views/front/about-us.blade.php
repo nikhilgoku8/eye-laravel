755924d5d638ec781138edd6dac5c3d0c28740ed
nikhilgoku8 <nikhilgoku8@gmail.com>
2025-12-17 12:31:33 +0530

    ABout page

--- resources/views/front/about-us.blade.php
+++ resources/views/front/about-us.blade.php
@@ -96,6 +96,110 @@
 </div>
 <!-- mission_vision_values -->
 
+<div class="founders">
+    <div class="container">
+        <div class="inner_container">
+            
+            <div class="heading center">Know about our founders</div>
+
+            <div class="info_boxes">
+                <div class="info_box">
+                    <div class="img_box">
+                        <img src="{{ asset('front/images/founders/dr-amjad-umer-furniturewala.png') }}">
+                    </div>
+                    <div class="text_box">
+                        <div class="title">Dr. Amjad Umer Furniturewala </div>
+                        <div class="sub_title">Medical Director, Orbit Eye Hospital</div>
+                        <p>Dr. Amjad Umer Furniturewala is the Proprietor and Medical Director of Orbit Eye Hospital, one of Mumbai's premier eye care institutions. A Gold Medalist in the Diploma in Ophthalmic Medicine and Surgery (DOMS) and holding qualifications including MS Ophthalmology from the UK and FRCS from the Royal College of Surgeons, Dr. Amjad is widely recognized for his extensive expertise in cataract and refractive surgeries. With over 20,000 cataract surgeries and more than 10,000 LASIK procedures performed, he has vast experience in advanced laser eye surgeries including SMILE. He is skilled in managing complex ophthalmic trauma cases, including intraocular foreign bodies, chemical burns, and corneo-scleral tear repairs.</p>
+                        <p>Dr. Amjad is also proficient in lacrimal sac surgeries, oculoplasty, strabismus correction, keratoplasty, glaucoma filtering surgeries, and administering intravitreal injections for the management of endophthalmitis. He leads a competent team at Orbit Eye Hospital, committed to employing cutting-edge technologies and maintaining the highest standards of patient care.</p>
+                        <p>Under his leadership, Orbit Eye Hospital was the first center in Maharashtra to introduce the Smart Pulse technology for touchless LASIK and has been awarded for excellence in eye care by prominent authorities.</p>
+                    </div>
+                </div>
+                <div class="info_box">
+                    <div class="img_box">
+                        <img src="{{ asset('front/images/founders/dr-lakshmi-hari-bipin-kattana.png') }}">
+                    </div>
+                    <div class="text_box">
+                        <div class="title">Dr. Lakshmi Hari Bipin Kattana</div>
+                        <div class="sub_title">Anterior Segment & Cornea Specialist</div>
+                        <p>Dr. Lakshmi Hari Bipin Kattana is a seasoned ophthalmologist at Orbit Eye Hospital with over 15 years of expertise in anterior segment and corneal diseases. Holding an MS degree, DNB qualifications, and a Fellowship in Anterior Segment and Cornea, she brings deep knowledge and skill to her practice.</p>
+                        <p>She expertly performs a variety of surgeries including cataract extraction, LASIK refractive surgery, pterygium removal, corneal transplantation, and repair of corneal and scleral tears. Dr. Lakshmi's patient-focused care combines advanced surgical techniques with compassionate treatment, ensuring the best possible outcomes.</p>
+                        <p>As a dedicated member of the Orbit Eye Hospital team, Dr. Lakshmi continually updates her training to incorporate the latest innovations, positioning her as a reliable and trusted doctor for complex anterior segment issues.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- founders -->
+
+<div class="added_feature_boxes">
+    <div class="container">
+        <div class="inner_container">
+            
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/experience.png') }}"></div>
+                <div class="text">19+ Years of Dedicated<br> Eye Care Service</div>
+            </div>
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/surgeries.png') }}"></div>
+                <div class="text">Over 50,000 Successful<br> Surgeries Performed</div>
+            </div>
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/technology.png') }}"></div>
+                <div class="text">State-of-the-art Facility <br> in Mumbai Suburbs</div>
+            </div>
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/team.png') }}"></div>
+                <div class="text">Highly Qualified  <br> Specialist Team</div>
+            </div>
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/ratings.png') }}"></div>
+                <div class="text">High Patient  <br> Satisfaction Ratings</div>
+            </div>
+            <div class="info_box">
+                <div class="icon"><img src="{{ asset('front/images/icons/insurance.png') }}"></div>
+                <div class="text">Affiliated With Major  <br> Insurance Providers</div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- feature_boxes -->
+
+<div class="why_trust">
+    <div class="container">
+        <div class="inner_container">
+
+            <div class="heading">Why Patients Trust Us</div>
+            
+            <div class="info_boxes">
+                <div class="info_box">
+                    <div class="text">Experienced medical team <br> trained at premier institutions</div>
+                </div>
+                <div class="info_box">
+                    <div class="text">Advanced infrastructure with latest <br> diagnostic and surgical equipment</div>
+                </div>
+                <div class="info_box">
+                    <div class="text">Comprehensive care from <br> pediatric to geriatric patients</div>
+                </div>
+                <div class="info_box">
+                    <div class="text">Thousands of <br> successful surgeries</div>
+                </div>
+                <div class="info_box">
+                    <div class="text">Surgeries annually with <br> excellent outcomes</div>
+                </div>
+                <div class="info_box">
+                    <div class="text">Patient-centric service <br> focus and affordable care</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- why_trust -->
+
 @include('front.includes.get-in-touch')
 
 </div>
